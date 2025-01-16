fd874fb6398ab10481f5cdce367f8a9b58baeb3d
KK04 <72666836+krzysztof-kopel@users.noreply.github.com>
2025-01-16 15:28:01 +0100

    Basic data structure ready

---
+++ MultinomialNaiveBayesClassifier.py
@@ -0,0 +1,36 @@
+class Trait:
+    def __init__(self, name: str, classes: list[str], possible_values: list[str]):
+        """Początkowo self.probabilities będzie liczyło, ile jest grzybów z danej klasy (trujący/jadalny) o danej wartości
+        cechy. Liczenie zaczynamy od 1, aby każda wartość cechy miała niezerowe prawdopodobieństwo (coś jak dodawanie czarnego
+        elementu w nagraniu ze StatQuest, chyba nazywa się to "Laplace smoothing").
+        Później, po wykonaniu funkcji calculate_probabilities self.probabilities[cls][trait_value] zwraca
+        prawdopodobieństwo należenia grzyba do klasy cls, przy założeniu, że grzyb wykazuje cechę o wartości trait_value."""
+        self.name = name
+        self.classes = classes
+        self.probabilities = {cls: {value: 1 for value in possible_values} for cls in classes}
+
+
+    def calculate_probabilities(self):
+        """Funkcja zamienia self.probabilities ze słownika liczącego, ile było grzybów danej klasy o danej wartości cechy,
+        na słownik prawdopodobieństw"""
+        for cls in self.classes:
+            sum_of_counts = sum(self.probabilities[cls].values())
+            for trait_value in self.probabilities[cls].keys():
+                self.probabilities[cls][trait_value] /= sum_of_counts
+
+
+class MultinomialNaiveBayesClassifier:
+    def __init__(self, classes: list[str], traits: list[Trait]):
+        self.classes = classes
+
+        self.traits = traits
+
+        self.class_probabilities = {cls: 0 for cls in classes}
+        # p(c_i), prawdopodobieństwo przynależności do danej klasy (a priori)
+
+    def calculate_class_probabilities(self):
+        """Funkcja podobna do Trait.calculate_probabilities - zamienia class_probabilities ze słownika zliczającego
+        wystąpienia poszczególnych klas na słownik prawdopodobieństw"""
+        sum_of_counts = sum(self.class_probabilities.values())
+        self.class_probabilities = {cls: value / sum_of_counts for cls, value in self.class_probabilities}
+
